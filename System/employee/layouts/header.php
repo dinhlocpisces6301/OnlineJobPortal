f66371c3badae57487837d6c54b24f9fb2b5892d
dinhlocpisces6301 <dinhloc334@gmail.com>
2022-05-15 23:08:38 +0700

    commit3

---
+++ System/employee/layouts/header.php
@@ -0,0 +1,44 @@
+			<nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function">
+				<div class="container">
+					
+					<div class="logo-wrapper">
+						<div class="logo">
+							<a href="../"><img src="../images/logo.png" alt="Logo" /></a>
+						</div>
+					</div>
+					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">
+						<ul class="nav navbar-nav" id="responsive-menu">
+							<li>
+								<a href="../">Trang chủ</a>
+							</li>
+							
+							<li>
+								<a href="../job-list.php">Danh sách</a>
+							</li>
+							
+							<li>
+								<a href="../employers.php">Nhà tuyển dụng</a>
+							</li>
+							
+							<li>
+								<a href="../employees.php">Ứng viên</a>
+							</li>
+							
+							<li>
+								<a href="../contact.php">Liên hệ</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="nav-mini-wrapper">
+						<ul class="nav-mini sign-in">
+							<li><a href="../logout.php">Đăng xuất</a></li>
+							<li><a href="./">Hồ sơ</a></li>
+						</ul>
+					</div>
+				</div>
+				
+				<div id="slicknav-mobile"></div>
+			</nav>
+
+			
--- System/employee/view-certificate-c.php
+++ System/employee/view-certificate-c.php
@@ -11,50 +11,38 @@
 
 <body>
 <?php
-require '../constants/db_config.php';
-require 'constants/check-login.php';
-$file_id = $_GET['id'];
+    require '../constants/db_config.php';
+    require 'constants/check-login.php';
+    $file_id = $_GET['id'];
 
-if ($user_online == "true") {
-if ($myrole == "employee") {
-}else{
-header("location:../");		
-}
-}else{
-header("location:../");	
-}
+    if ($user_online == "true") {
+        if ($myrole == "employee") {
+        } else {
+            header("location:../");		
+        }
+    } else {
+        header("location:../");	
+    }
 
-try {
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+        $stmt = $conn->prepare("SELECT * FROM tbl_academic_qualification WHERE id = :fileid  AND member_no = '$myid'");
+        $stmt->bindParam(':fileid', $file_id);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
 
-	
-$stmt = $conn->prepare("SELECT * FROM tbl_academic_qualification WHERE id = :fileid  AND member_no = '$myid'");
-$stmt->bindParam(':fileid', $file_id);
-$stmt->execute();
-$result = $stmt->fetchAll();
-
-foreach($result as $row)
-{
-    $certificate = $row['certificate'];
-	$transcript = $row['transcript'];
-	
-	?>
-	<div style="width:100%">
-    <iframe  style="border:none;" src="../ViewerJS/?title=CERTIFICATE#<?php echo 'data:application/pdf;base64,'.base64_encode($certificate).'' ?>" height="100%" width="100%"></iframe>
-    <iframe style="border:none;" src="../ViewerJS/?title=TRANSCRIPT#<?php echo 'data:application/pdf;base64,'.base64_encode($transcript).'' ?>" height="100%" width="100%"></iframe>
-    </div>
-
-<?php
-}
-
-					  
-}catch(PDOException $e)
-{
-
-}
-
-?>
+        foreach($result as $row)
+        {
+            $certificate = $row['certificate'];
+            $transcript = $row['transcript']; ?>
+            <div style="width:100%">
+                <iframe  style="border:none;" src="../ViewerJS/?title=CERTIFICATE# <?= 'data:application/pdf;base64,'.base64_encode($certificate).'' ?>" height="100%" width="100%"></iframe>
+                <iframe style="border:none;" src="../ViewerJS/?title=TRANSCRIPT# <?= 'data:application/pdf;base64,'.base64_encode($transcript).'' ?>" height="100%" width="100%"></iframe>
+            </div>
+        <?php }
+    } catch(PDOException $e){} ?>
 </body>
 
 </html>
